69f32a416d991166b12425bdb52c69dca982e7bf
Byron Morales <byronmoralesveliz@Byrons-MacBook-Pro.local>
2026-01-08 20:36:23 +0100

    feat (INFRA): created terraform files for app cdn

---
+++ infra/www/pbrand/data.tf
@@ -0,0 +1,11 @@
+/*
+data "terraform_remote_state" "acm" {
+  backend = "s3"
+  config = {
+    bucket  = var.tf_bucket_name
+    key     = var.cnmanage_key
+    region  = var.region
+    profile = var.profile
+  }
+}
+*/
---
+++ infra/www/pbrand/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  //aliases             = try(data.terraform_remote_state.acm.outputs.domain_name, var.aliases)
+  aliases             = var.aliases
+  //acm_certificate_arn = try(data.terraform_remote_state.acm.outputs.certification_arn, var.acm_certification)
+  acm_certificate_arn = var.acm_certification
+  //parent_zone_id      = try(data.terraform_remote_state.acm.outputs.route_53_zone_id, var.route_53_zone_id)
+  parent_zone_id      = var.route_53_zone_id
+}
